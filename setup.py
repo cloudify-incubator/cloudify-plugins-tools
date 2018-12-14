6158c744178ef06c712a05dc7846462f9313d877
Krzysztof Bijakowski <krzysztof.bijakowski@cloudify.co>
2018-12-14 09:25:52 +0100

    Dependencies using requirements

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 from setuptools import setup
 
-# TODO use cloudify-utilities-plugin-sdk when it will be released
-REST_PLUGIN_URL = 'https://github.com/cloudify-incubator/' \
-                  'cloudify-utilities-plugin/archive/1.10.0.tar.gz' \
-                  '#egg=cloudify-utilities-plugin-1.10.0'
-
-try:
-    import cloudify_rest  # noqa
-except ImportError:
-    import pip
-    if hasattr(pip, 'main'):
-        pip.main(['install', '"{}"'.format(REST_PLUGIN_URL)])
-    else:
-        from pip._internal import main as _main
-        _main(['install', '"{}"'.format(REST_PLUGIN_URL)])
-
 setup(
     name='cloudify-plugins-tools',
     version='0.1.0',
@@ -32,6 +17,5 @@ setup(
     install_requires=[
         'cloudify-plugins-common>=4.2',
         'cloudify-utilities-plugin==1.10.0'
-    ],
-    dependency_links=[REST_PLUGIN_URL]
+    ]
 )
